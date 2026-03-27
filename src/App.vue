3c1d8fbf59c370b39a0b0000069d85c50522a011
zhaoshanyu <zhaoshanyu@yodafone.cn>
2026-03-27 15:11:41 +0800

    fix:解决未用问题

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,18 @@
 <script setup>
 const info = {
-  name: "zsy",
-
-
+  name: 'zsy',
 
   age: 18
-
-
-
-
-}
+};
+console.log(info);
 </script>
 
 <template>
   <h1>You did it!</h1>
   <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
+    Visit
+    <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to
+    read the documentation
   </p>
 </template>
 
